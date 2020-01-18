f4c9d00e9913240cda20380155ce676693f21557
alibekkrona <56386591+alibekkrona@users.noreply.github.com>
2020-01-18 11:30:03 +0200

    Add lib to project. Moved lib to another git

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once './vendor/addressBook/config/config.php';
+require_once './vendor/arsenalibek/addressbook/config/config.php';
+require_once './vendor/autoload.php';
 
-use addressBook\AddressBook;
+//use addressBook\AddressBook;
 use addressBook\Person;
 use addressBook\Group;
+
 
 /**
  * Init address book with config pre set data.
@@ -15,7 +17,8 @@ use addressBook\Group;
  * Init address book without config and Add new Person
  * Example of person exists in the config.php
  */
-$addressBook = new AddressBook();
+
+$addressBook = new addressBook\AddressBook();
 $personConfig = [
     'id' => '1',
     'firstName' => 'David A',
@@ -39,49 +42,71 @@ $personConfig = [
 ];
 $person = new Person($personConfig);
 $addressBook->addPerson($person);
+echo 'Added one persons: <pre>';
+print_r($addressBook->getPersons());
 
 /**
  * Add new group to AddressBook
  * Example get from config/config.php
  */
+/*
 $addressBook = new AddressBook();
 $groupConfig = [
-    '1' => [
-        'id' => '1',
-        'name' => 'Group1'
-    ],
+    'id' => '1',
+    'name' => 'Group1'
 ];
 $group = new Group($groupConfig);
 $addressBook->addGroup($group);
 
+echo "Added one group <pre>:";
+print_r($addressBook->getGroups());
+exit(0);
+*/
 /**
  * Given a group we want to easily find its members.
  */
+/*
 $addressBook = new AddressBook($config);
 $groupId = '1';
 $persons = $addressBook->findPersonsByGroup($groupId);
+echo 'Find persons belongs group: <pre>';
 print_r($persons);
+exit(0);
+*/
 
 /**
  * Given a person we want to easily find the groups the person belongs to.
  */
+/*
 $addressBook = new AddressBook($config);
-$groupId = '1';
-$groups = $addressBook->findGroupsByPerson($groupId);
+$personId = '2';
+$groups = $addressBook->findGroupsByPerson($personId);
+echo 'Find group person belongs: <pre>';
+print_r($groups);
+exit(0);
+*/
 
 /**
  * Find person by name (can supply either first name, last name, or both).
  */
+/*
 $addressBook = new AddressBook($config);
-$persons = $addressBook->findPersonByName('John');
+$persons = $addressBook->findPersonsByName('John');
+echo 'Find persons for query word: <pre>';
 print_r($persons);
+exit(0);
+*/
 
 /**
  * Find person by email address
  */
+/*
 $addressBook = new AddressBook($config);
 $persons = $addressBook->findPersonByEmail('john');
+echo 'Find persons by email: <pre>';
 print_r($persons);
+exit(0);
+*/
 
 
 
